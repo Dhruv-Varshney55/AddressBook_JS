093e1b5b0fa4d69ed6a49ceda33faad3476a802c
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-03-22 23:59:18 +0530

    UC1

---
+++ AddressBook.js
@@ -0,0 +1,23 @@
+// UC1 (Create Contact)
+
+class Contact{
+    firstName;
+    lastName;
+    address;
+    state;
+    city;
+    zipCode;
+    email;
+    phoneNumber;
+
+    constructor(firstName, lastName, address, state, city, zipCode, email, phoneNumber){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.address = address
+        this.state = state
+        this.city = city
+        this.zipCode = zipCode
+        this.email = email
+        this.phoneNumber = phoneNumber
+    }
+}
